241a592cce282c81ec6e4a9bb78d35bbb2d6fa91
Aryan Mehta <aryanmehta@Aryans-MacBook-Air.local>
2021-12-27 17:43:51 +0000

    starting project

---
+++ src/components/header/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+function Header() {
+    return (
+        <div>
+            <div className="logo">
+                <span>The Planets</span>
+            </div>
+            <div className="nav-bar"></div>
+        </div>
+    )
+}
+
+export default Header
